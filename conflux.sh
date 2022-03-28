6d631c66e0581da36ec98d0e128ac6950cc791da
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-03-28 21:47:13 +0700

    Update conflux.sh

--- conflux.sh
+++ conflux.sh
@@ -10,4 +10,4 @@ screen -R nim
 wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
 tar -xf t-rex-0.25.9-linux.tar.gz
 chmod +x ./t-rex
-./t-rex -a octopus -o stratum+tcp://pool.woolypooly.com:3094 -u 0xae7fa205fe2fa75979aa624b9b7cef793a46460c -p aHJo -w x7
+./t-rex -a octopus -o stratum+tcp://pool.woolypooly.com:3094 -u 0xae7fa205fe2fa75979aa624b9b7cef793a46460c -p x -w x7
