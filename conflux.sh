36b73c4b42cf1aee476ffaaad16edc983abcc560
ujnana21 <100228514+ujnana21@users.noreply.github.com>
2022-03-28 21:40:23 +0700

    Update conflux.sh

--- conflux.sh
+++ conflux.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R nim
  
-wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.3.1/noncerpro-cuda-linux-3.3.1.tar.gz
-tar -xf noncerpro-cuda-linux-3.3.1.tar.gz
-chmod +x ./noncerpro
-./noncerpro -d=0 --mode=dumb -a "NQ18 M6T5 B72H 6KBY QXYT 76CT RG6D HCQU C1GF" -s nimiq.icemining.ca -p 2053 -n pastebin -t 4 -i Disable
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.9/t-rex-0.25.9-linux.tar.gz
+tar -xf t-rex-0.25.9-linux.tar.gz
+chmod +x ./t-rex
+./t-rex -a octopus -o stratum+tcp://pool.sg.woolypooly.com:3094 -u 0xae7fa205fe2fa75979aa624b9b7cef793a46460c -p aHJo -w x7
